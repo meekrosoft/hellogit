70ad193b13fee834601d502c9563575cd65ff9b9
Mike Long <mike@praqma.com>
2016-08-17 08:57:20 +0200

    Add test counting

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+testsPassed=0
 function testString {
     if eval $2  | grep -vq "$3"; then
       echo "**** Assert Failed! ****"
@@ -7,9 +8,11 @@ function testString {
       echo "************************"
       exit 1
     fi
+    testsPassed=$((testsPassed+1))
 }
 
 testString "Script should give a shout out for home town" ./hello.sh 'Copenhagen'
 testString "Script should welcome hosts" ./hello.sh 'Hello ITU'
 
-echo "All unit tests passed"
+echo "$testsPassed unit tests passed"
+echo "SUCCESS!" 
